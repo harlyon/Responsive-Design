ee75bfb09c6a56850edc2c79087ba497bd63b7c6
Harrison Ekpobimi <okeharlyon@gmail.com>
2017-11-28 14:05:49 +0200

    initial commit

---
+++ PSD - HTML/dev/scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(function () {
+	$('a').smoothScroll({
+		offset: 0,
+		speed: 1000,
+		easing: 'swing'
+	});
+
+	$('.hamburger').on('click', function(){
+		$('.menu').toggleClass('overlay');
+		$('.mobileMenu').toggleClass('open');
+	})
+});
